36bed86d5010940138a3d69ddc98f663fe067330
Binod-Perera <108719707+BinodPerera@users.noreply.github.com>
2023-08-05 20:26:38 +0530

    Add files via upload

---
+++ QUESTIONS/20.3/3. a./2.php
@@ -0,0 +1,25 @@
+<?php
+
+    //getting values from form
+    $fname = $_REQUEST["fname"];
+    $lname = $_REQUEST["lname"];
+    $mobileNo = $_REQUEST["telNo"];
+    $gender = $_REQUEST["gender"];
+    $email = $_REQUEST["email"];
+    $additional = $_REQUEST["additional"];
+    $music = $_REQUEST["music"];
+    $condition = $_REQUEST["condition"];
+    
+
+    echo "First Name: " ,$fname,"<br>";
+    echo "Last Name: ", $lname,"<br>";
+    echo "Mobile No: ",$mobileNo, "<br>";
+    echo "Gender: ", $gender,"<br>";
+    echo "Email: ", $email, "<br>";
+    echo "Additional Data: ", $additional,"<br>";
+    echo "Music: ";
+    foreach ($music as $e){
+        echo $e," / ";
+    }
+    echo "<br>Tearms & conditions: ",$condition,"<br>";
+?>
